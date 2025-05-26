ad099af52c42620343149fb6e5a3377aba03221a
GGB0T11 <gusttrabs@gmail.com>
2025-05-26 20:58:08 -0300

    tokyo night theme

---
+++ tokyo_night/.config/nvim/lua/config/theme.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+M.nvim_colorscheme = "tokyonight"
+
+M.lualine_theme = "tokyonight"
+
+return M
---
+++ tokyo_night/.config/nvim/lua/plugins/appearance.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("tokyonight").setup({
+                style = "night",
+                transparent = true,
+                terminal_colors = true,
+                styles = {
+                    comments = { italic = true },
+                    keywords = { italic = true },
+                    functions = {},
+                    variables = {},
+                },
+            })
+        end,
+    },
+}
