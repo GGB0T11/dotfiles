06a31efd97fb684c8ddf0067edca96311f0fc30b
GGB0T11 <gusttrabs@gmail.com>
2025-05-27 16:49:29 -0300

    catppuccin theme

---
+++ catppuccin/.config/nvim/lua/config/theme.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+M.nvim_colorscheme = "catppuccin"
+
+M.lualine_theme = "catppuccin"
+
+return M
---
+++ catppuccin/.config/nvim/lua/plugins/appearance.lua
@@ -0,0 +1,28 @@
+return{
+    {
+    "catppuccin/nvim", 
+    name = "catppuccin", 
+    priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha",
+                transparent_background = true,
+                show_end_of_buffer = false,
+                term_colors = true,
+                styles = {
+                    comments = { "italic" },
+                    conditionals = { "italic" },
+                },
+                default_integrations = true,
+                integrations = {
+                    cmp = true,
+                    gitsigns = true,
+                    nvimtree = true,
+                    treesitter = true,
+                    notify = true,
+                    mini = true,
+                    },
+            })
+        end, 
+    } 
+}
