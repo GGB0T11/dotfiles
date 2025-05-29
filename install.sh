c1744e36de7ac51a8234884767eaf9bc7f369ed6
GGB0T11 <gusttrabs@gmail.com>
2025-05-29 17:29:09 -0300

    new install script (no tested)

--- install.sh
+++ install.sh
@@ -1,91 +1,109 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/bash
 
 info() { echo -e "\e[1;34m[INFO]\e[0m $*"; }
 success() { echo -e "\e[1;32m[SUCCESS]\e[0m $*"; }
 error() { echo -e "\e[1;31m[ERROR]\e[0m $*"; }
 
-if [[ ! -f /etc/arch-release ]]; then
-    error "This script only works on Arch-based distros."
-    exit 1
-fi
-
-for dir in hypr hyprpanel kitty fastfetch wofi; do
-    target="$HOME/.config/$dir"
-    if [[ -e "$target" && ! -L "$target" ]]; then
-        error "$target already exists, remove or backup it."
+verify_distro() {
+    if [[ ! -f /etc/arch-release ]]; then
+        error "This script only works on Arch-based distros."
         exit 1
     fi
-done
+}
 
-if [[ -e "$HOME/Wallpapers" && ! -L "$HOME/Wallpapers" ]]; then
-    error "$HOME/Wallpapers already exists, remove or backup it."
-    exit 1
-fi
-
-for file in .zshrc .p10k.zsh; do
-    target="$HOME/$file"
-    if [[ -e "$target" && ! -L "$target" ]]; then
-        error "$target already exists, remove or backup it."
-        exit 1
+verify_dots() {
+    if [[ ! -d "$HOME/dotfiles" ]]; then
+        info "Cloning dotfiles $HOME..."
+        git clone https://github.com/GGB0T11/dotfiles.git "$HOME"
+        success "Dotfiles cloned."
+    else
+        info "Dotfiles already cloned."
     fi
-done
+}
 
-if ! command -v yay &>/dev/null; then
-    info "Installing Yay..."
-    sudo pacman -S yay --noconfirm
-    success "Yay installed."
-fi
+install_pkgs() {
+    PACMAN_PKGS=(
+        git stow kitty swww hyprland hyprlock kitty zsh neovim
+        btop superfile wofi fastfetch wpctl brightnessctl
+        playerctl grimblast bluez bluez-utils networkmanager
+        power-profiles-daemon
+        )
 
-info "Installing essentials..."
-sudo pacman -S --needed --noconfirm git stow swww hyprland kitty zsh neovim wget curl
-yay -S --noconfirm hyprpanel
-success "Essentials installed."
+    YAY_PKGS=(
+        ags-hyprpanel-git brave-bin nerd-fonts-fira-code
+        )
 
-if [[ ! -d "$HOME/dotfiles" ]]; then
-    info "Cloning dotfiles..."
-    git clone https://github.com/GGB0T11/dotfiles.git ~/
-    success "Dotfiles cloned."
-else
-    info "Dotfiles already cloned."
-fi
+    if ! command -v yay &>/dev/null; then
+        info "Yay not found, installing..."
+        sudo pacman -S yay --noconfirm
+        success "Yay installed."
+    fi
+    info "Installing pacman packages..."
+    sudo pacman -S --noconfirm --needed "${PACMAN_PKGS[@]}"
+    
+    info "Installing yay packages"
+    yay -S --noconfirm --needed "${YAY_PKGS[@]}"
+}
 
-info "Installing fonts..."
-yay -S nerd-fonts-fira-code
-success "Fonts installed."
+verify_targets() {
+    BACKUP_DIR="$HOME/dotbackup"
+    mkdir $BACKUP_DIR
 
-info "Available themes:"
-themes=($(find "$HOME/dotfiles" -mindepth 1 -maxdepth 1 -type d -exec basename {} \ | grep -vE '^(base|scripts)$'))
+    for dir in hypr hyprpanel kitty fastfetch wofi; do
+        target="$HOME/.config/$dir"
+        if [[ -e "$target" && ! -L "$target" ]]; then
+            echo "Moving $target to $BACKUP_DIR"
+            mv "$target" "$BACKUP_DIR/"
+        fi
+    done
+    
+    if [[ -e "$HOME/Wallpapers" && ! -L "$HOME/Wallpapers" ]]; then
+        echo "Moving $target to $BACKUP_DIR"
+        mv "$target" "$BACKUP_DIR/"
+    fi
+    
+    for file in .zshrc .p10k.zsh; do
+        target="$HOME/$file"
+        if [[ -e "$target" && ! -L "$target" ]]; then
+            echo "Moving $target to $BACKUP_DIR"
+            mv "$HOME/Wallpapers" "$BACKUP_DIR/"
+        fi
+    done
+}
 
-echo
-for i in "${!themes[@]}"; do
-    echo "[$i] ${themes[$i]}"
-done
-echo
+configure_services() {
+    sudo systemctl disable systemd-resolved
+    sudo systemctl disable systemd-networkd
+    sudo systemctl enable NetworkManager
+    sudo systemctl start NetworkManager   
+    sudo systemctl enable bluetooth
+    sudo systemctl start bluetooth
+}
 
-read -rp "Select a theme by number: " theme_index
+apply_dot() {
+    info "Applying base dotfiles..."
+    stow -d "$HOME/dotfiles" base
+    success "Base applied."
+    
+    info "Applying the default theme..."
+    bash "$HOME/dotfiles/swap-theme.sh default"
+    success "Theme applied."
+    
+    if [[ "$SHELL" != "$(which zsh)" ]]; then
+        info "Changing default shell to ZSH..."
+        chsh -s "$(which zsh)"
+        success "Shell changed."
+    fi
+}
 
-if [[ ! "$theme_index" =~ ^[0-9]+$ ]] || (( theme_index < 0 || theme_index >= ${#themes[@]} )); then
-    error "Invalid selection."
-    exit 1
-fi
+main() {
+    verify_distro
+    verify_dots
+    install_pkgs
+    verify_targets
+    configure_services
+    apply_dot
+}
 
-THEME_DEFAULT="${themes[$theme_index]}"
-info "Theme selected: $THEME_DEFAULT"
-
-info "Applying base dotfiles..."
-stow -d "$HOME/dotfiles" base
-success "Base applied."
-
-info "Applying theme '$THEME_DEFAULT'..."
-bash "$HOME/dotfiles/scripts/swap-theme.sh" "$THEME_DEFAULT"
-success "Theme applied."
-
-if [[ "$SHELL" != "$(which zsh)" ]]; then
-    info "Changing default shell to ZSH..."
-    chsh -s "$(which zsh)"
-    success "Shell changed."
-fi
-
-success "Installation completed. Restart your PC."
+main
 
